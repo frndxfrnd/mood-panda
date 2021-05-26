efa2096be3690425a90978a5ba5c3b539968ece2
magabyt3 <maggieshao27@gmail.com>
2021-05-26 17:50:08 -0400

    spotify auth

--- functions/src/spotify.js
+++ functions/src/spotify.js
@@ -27,9 +27,36 @@ const auth = axios.create({
   baseURL: 'https://accounts.spotify.com'
 })
 
+app.use(async (req, res, next) => {
+  if (req.session?.expires_at && Date.now() >= req.session.expires_at) {
+    try {
+      const result = await auth.post('/api/token', encodeURI(`grant_type=refresh_token&refresh_token=${req.session.refresh_token}`), {
+        headers: {
+          Authorization: `Basic ${Buffer.from(`${config().spotify.id}:${config().spotify.key}`).toString('base64')}`,
+          'content-type': 'application/x-www-form-urlencoded'
+        }
+      })
+
+      req.session.access_token = result.data.access_token
+      req.session.expires_at = Date.now() + result.data.expires_in * 1000
+    } catch (e) {
+      console.error(e)
+      if (e.response) {
+        res.status(e.response.status)
+        res.json(e.response.data)
+      } else {
+        res.status(500)
+        res.end()
+      }
+    }
+  }
+
+  next()
+})
+
 app.get('/login', async (req, res) => {
-  const state = crypto.randomBytes(20).toString('hex')
-  req.session.state = req.session.state || state
+  const state = req.session.state || crypto.randomBytes(20).toString('hex')
+  req.session.state = state
 
   res.redirect(encodeURI(`${auth.defaults.baseURL}/authorize?client_id=${config().spotify.id}&response_type=code&redirect_uri=http://localhost:5001/mood-panda/us-central1/spotify/callback&state=${state}&scope=user-read-email user-read-recently-played&show_dialog=false`))
 })
@@ -37,6 +64,7 @@ app.get('/login', async (req, res) => {
 app.get('/callback', async (req, res) => {
   const code = req.query.code
   if (req.query.state !== req.session.state) {
+    res.status(401)
     res.end()
     return
   }
@@ -51,11 +79,18 @@ app.get('/callback', async (req, res) => {
 
     req.session.access_token = result.data.access_token
     req.session.refresh_token = result.data.refresh_token
+    req.session.expires_at = Date.now() + result.data.expires_in * 1000
 
     res.json(result.data)
   } catch (e) {
-    console.log(e)
-    res.end()
+    console.error(e)
+    if (e.response) {
+      res.status(e.response.status)
+      res.json(e.response.data)
+    } else {
+      res.status(500)
+      res.end()
+    }
   }
 })
 
@@ -81,7 +116,13 @@ app.get('/history', async (req, res) => {
     res.json(audioFeatures.data.audio_features.map((e, i) => ({ id: ids[i], audio_features: e })))
   } catch (e) {
     console.error(e)
-    res.end()
+    if (e.response) {
+      res.status(e.response.status)
+      res.json(e.response.data)
+    } else {
+      res.status(500)
+      res.end()
+    }
   }
 })
 
